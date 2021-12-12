713db0fa0854085abcae2bc1ccdee29081450785
Koji <koji.1127.403@outlook.jp>
2021-12-12 18:57:52 +0900

    removed environ

--- dclass/settings.py
+++ dclass/settings.py
@@ -17,7 +17,6 @@ from dotenv import (
     load_dotenv,
 )
 import os 
-import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
