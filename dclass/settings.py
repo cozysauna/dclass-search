5ae1f2c69a2dfc25f178fa18c7be45254a633b01
Koji <koji.1127.403@outlook.jp>
2021-12-12 18:52:31 +0900

    modified small error

--- dclass/settings.py
+++ dclass/settings.py
@@ -23,7 +23,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
