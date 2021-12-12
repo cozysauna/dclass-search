c002906091ae05b599cf137333aecd9f085e3444
Koji <koji.1127.403@outlook.jp>
2021-12-12 11:34:39 +0900

    modified .env

--- dclass/settings.py
+++ dclass/settings.py
@@ -17,20 +17,25 @@ from dotenv import (
     load_dotenv,
 )
 import os 
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+env = environ.Env()
+env.read_env(os.path.join(BASE_DIR, '.env'))
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = False if env('DEBUG') == 'False' else True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-03wmqk=oqnvmnh$)8kdf+*awi$n8x44ir7qshfg7qd&19nyb7g'
+# SECRET_KEY = 'django-insecure-03wmqk=oqnvmnh$)8kdf+*awi$n8x44ir7qshfg7qd&19nyb7g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
@@ -88,6 +93,7 @@ if DEBUG: # 開発
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+
 else: # 本番
     load_dotenv(find_dotenv())
     DATABASES = {
